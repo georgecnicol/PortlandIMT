55d373df6a87d53bbc5d52721bb692f54dc62b33
George Nicol <piratezpdx@gmail.com>
2020-08-04 17:31:23 -0700

    continue refactor for aws deployment

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-#from project import app
 from project import create_app
-
-# this needs to look more like Corey's code. You can't have all that stuff
-# under if name == main. I think gunicorn gets passed an attirbute from above there...
-
 
 pimt_app = create_app()
 
